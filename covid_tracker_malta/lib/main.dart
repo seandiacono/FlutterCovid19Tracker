f85d7cd01e4c64330cf8685525a4ed0775ff700f
seandiacono <seandiacono@gmail.com>
2020-04-22 18:38:07 +0200

    + Added refresh and change in numbers to UI

--- covid_tracker_malta/lib/main.dart
+++ covid_tracker_malta/lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class CovidNumbers {
+  int newConfirmed;
+  int newRecovered;
+  int newCritcal;
+  int newDeaths;
   final int confirmed;
   final int recovered;
   final int critical;
@@ -33,7 +38,43 @@ Future<CovidNumbers> fetchData(http.Client client) async {
     },
   );
   if (response.statusCode == 200) {
-    return CovidNumbers.fromJson(json.decode(response.body)[0]);
+    var data = CovidNumbers.fromJson(json.decode(response.body)[0]);
+    final prefs = await SharedPreferences.getInstance();
+
+    final oldConfirmed = prefs.getInt('confirmed') ?? 0;
+    final oldRecovered = prefs.getInt('recovered') ?? 0;
+    final oldCritical = prefs.getInt('critical') ?? 0;
+    final oldDeaths = prefs.getInt('deaths') ?? 0;
+
+    if (oldConfirmed != 0) {
+      data.newConfirmed = data.confirmed - oldConfirmed;
+    } else {
+      data.newConfirmed = 0;
+    }
+
+    if (oldRecovered != 0) {
+      data.newRecovered = data.recovered - oldRecovered;
+    } else {
+      data.newRecovered = 0;
+    }
+
+    if (oldCritical != 0) {
+      data.newCritcal = data.critical - oldCritical;
+    } else {
+      data.newCritcal = 0;
+    }
+
+    if (oldDeaths != 0) {
+      data.newDeaths = data.deaths - oldDeaths;
+    } else {
+      data.newDeaths = 0;
+    }
+
+    prefs.setInt('confirmed', data.confirmed);
+    prefs.setInt('recovered', data.recovered);
+    prefs.setInt('critical', data.critical);
+    prefs.setInt('deaths', data.deaths);
+    return data;
   } else {
     throw Exception('Failed to load Data');
   }
@@ -47,10 +88,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: 'RobotoSlab'
-      ),
+          primarySwatch: Colors.grey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'RobotoSlab'),
       home: MyHomePage(title: 'COVID-19 Malta'),
     );
   }
@@ -66,76 +106,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _getData() async {
+    setState(() {
+      fetchData(http.Client());
+    });
+  }
+
+  Widget _ListTile(
+      String iconPath, String name, String number, String newNumber) {
+    return Card(
+      child: Center(
+        child: ListTile(
+          title: Text(name,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+          leading: Image.asset(iconPath),
+          trailing: RichText(
+            text: TextSpan(
+              text: '',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              children: <TextSpan>[
+              TextSpan(
+                text: number,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, color: Colors.black),
+              ),
+              TextSpan(
+                  text: ' +' + newNumber,
+                  style: TextStyle(fontSize: 24, color: Colors.green))
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text(widget.title, style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+          title: Text(widget.title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold)),
           backgroundColor: Colors.grey[800],
         ),
-        body: FutureBuilder<CovidNumbers>(
-          future: fetchData(http.Client()),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView(
-                children: <Widget>[
-                  Container(
-                    height: 175,
-                    child: Card(
-                      child: Center(
-                        child: ListTile(
-                          title: Text('Confirmed Cases', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                          leading: Image.asset('assets/fever.png'),
-                          trailing: Text(snapshot.data.confirmed.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 175,
-                    child: Card(
-                      child: Center(
-                        child: ListTile(
-                          title: Text('Recovered', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                          leading: Image.asset('assets/recovered.png'),
-                          trailing: Text(snapshot.data.recovered.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 175,
-                    child: Card(
-                      child: Center(
-                        child: ListTile(
-                          title: Text('Deaths', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                          leading: Image.asset('assets/death.png'),
-                          trailing: Text(snapshot.data.deaths.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 175,
-                    child: Card(
-                      child: Center(
-                        child: ListTile(
-                          title: Text('Critical Condition', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                          leading: Image.asset('assets/ventilator.png'),
-                          trailing: Text(snapshot.data.critical.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
+        body: RefreshIndicator(
+          onRefresh: _getData,
+          child: FutureBuilder<CovidNumbers>(
+            future: fetchData(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView(
+                  children: <Widget>[
+                    Container(
+                        height: 175,
+                        child: _ListTile(
+                            'assets/fever.png',
+                            'Confirmed Cases',
+                            snapshot.data.confirmed.toString(),
+                            snapshot.data.newConfirmed.toString())),
+                    Container(
+                        height: 175,
+                        child: _ListTile(
+                            'assets/recovered.png',
+                            'Recovered',
+                            snapshot.data.recovered.toString(),
+                            snapshot.data.newRecovered.toString())),
+                    Container(
+                        height: 175,
+                        child: _ListTile(
+                            'assets/death.png',
+                            'Deaths',
+                            snapshot.data.deaths.toString(),
+                            snapshot.data.newDeaths.toString())),
+                    Container(
+                        height: 175,
+                        child: _ListTile(
+                            'assets/ventilator.png',
+                            'Critical Condition',
+                            snapshot.data.critical.toString(),
+                            snapshot.data.newCritcal.toString()))
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
 
-            return CircularProgressIndicator();
-          },
+              return CircularProgressIndicator();
+            },
+          ),
         ));
   }
 }

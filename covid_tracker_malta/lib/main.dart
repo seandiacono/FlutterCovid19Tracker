101cd0d758e17f0612d1a083bf86a63fdabba1a5
seandiacono <seandiacono@gmail.com>
2020-04-22 02:10:34 +0200

    + Connected to Covid-19 data API and displayed data

--- covid_tracker_malta/lib/main.dart
+++ covid_tracker_malta/lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class CovidNumbers {
+  final int confirmed;
+  final int recovered;
+  final int critical;
+  final int deaths;
+
+  CovidNumbers({this.confirmed, this.recovered, this.critical, this.deaths});
+
+  factory CovidNumbers.fromJson(Map<String, dynamic> json) {
+    return CovidNumbers(
+        confirmed: json['confirmed'],
+        recovered: json['recovered'],
+        critical: json['critical'],
+        deaths: json['deaths']);
+  }
+}
+
+Future<CovidNumbers> fetchData(http.Client client) async {
+  final response = await http.get(
+    'https://covid-19-data.p.rapidapi.com/country?format=json&name=malta',
+    headers: {
+      "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+      "x-rapidapi-key": "e89e9094a4msh8b4b495eb6ee17dp1202d3jsn05a547d69bdd",
+    },
+  );
+  if (response.statusCode == 200) {
+    return CovidNumbers.fromJson(json.decode(response.body)[0]);
+  } else {
+    throw Exception('Failed to load Data');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,24 +44,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'RobotoSlab'
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'COVID-19 Malta'),
     );
   }
 }
@@ -41,29 +66,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.title, style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+          backgroundColor: Colors.grey[800],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: FutureBuilder<CovidNumbers>(
+          future: fetchData(http.Client()),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                children: <Widget>[
+                  Container(
+                    height: 175,
+                    child: Card(
+                      child: Center(
+                        child: ListTile(
+                          title: Text('Confirmed Cases', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                          leading: Image.asset('assets/fever.png'),
+                          trailing: Text(snapshot.data.confirmed.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 175,
+                    child: Card(
+                      child: Center(
+                        child: ListTile(
+                          title: Text('Recovered', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                          leading: Image.asset('assets/recovered.png'),
+                          trailing: Text(snapshot.data.recovered.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 175,
+                    child: Card(
+                      child: Center(
+                        child: ListTile(
+                          title: Text('Deaths', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                          leading: Image.asset('assets/death.png'),
+                          trailing: Text(snapshot.data.deaths.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 175,
+                    child: Card(
+                      child: Center(
+                        child: ListTile(
+                          title: Text('Critical Condition', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                          leading: Image.asset('assets/ventilator.png'),
+                          trailing: Text(snapshot.data.critical.toString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            return CircularProgressIndicator();
+          },
+        ));
   }
 }
